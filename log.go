358289371b2c93a4fed34017dbfa85aae610f40e
Topher Marie <topherMarie@gmail.com>
2014-03-20 18:52:28 -0600

    Log enhancement to have a global

--- log.go
+++ log.go
@@ -30,30 +30,74 @@ type Log struct{
 	internal_logger lumber.Logger	
 }
 
+
 func NewLogger(o int) *Log {
 	return &Log{internal_logger:lumber.NewConsoleLogger(o)}
+}
+
+var std = NewLogger(FATAL)
+
+func (log *Log) Panic(message string){
+	log.internal_logger.Fatal(message)
+	panic(message)
+}
+
+func Panic(message string){
+	std.Panic(message)
 }
 
 func (log *Log) Fatal(message string){
 	log.internal_logger.Fatal(message)
 }
 
+func Fatal(message string){
+	std.Fatal(message)
+}
+
 func (log *Log) Error(message string){
 	log.internal_logger.Error(message)
+}
+
+func Error(message string){
+	std.Error(message)
 }
 
 func (log *Log) Warn(message string){
 	log.internal_logger.Warn(message)
 }
 
+func Warn(message string){
+	std.Warn(message)
+}
+
 func (log *Log) Info(message string){
 	log.internal_logger.Info(message)
+}
+
+func Info(message string){
+	std.Info(message)
 }
 
 func (log *Log) Debug(message string){
 	log.internal_logger.Debug(message)
 }
 
+func Debug(message string){
+	std.Debug(message)
+}
+
 func (log *Log) Trace(message string){
 	log.internal_logger.Trace(message)
+}
+
+func Trace(message string){
+	std.Trace(message)
+}
+
+func (log *Log) Level(o int) {
+	log.internal_logger.Level(o)
+}
+
+func Level(o int) {
+	std.Level(o)
 }
--- log_test.go
+++ log_test.go
@@ -9,3 +9,30 @@ func TestLevel(t *testing.T){
 
 	log.Fatal("this is some text")
 }
+
+func TestPanic(t *testing.T) {
+	log := NewLogger(FATAL)
+	defer func(){
+		if r := recover(); r != nil {
+
+		}else{
+			t.Error("Expected a panic")
+		}
+	}()
+
+	log.Panic("this is some text")
+}
+
+func TestStdLog(t *testing.T) {
+	Fatal("Fatal non-fatal message")
+	Level(DEBUG)
+
+	defer func(){
+		if r := recover(); r != nil {
+
+		}else{
+			t.Error("Expected a panic")
+		}
+	}()
+	Panic("Panic message")
+}
